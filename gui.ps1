14c06ef9f2cfef0250c3a90942d89fa4be6a9656
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2024-04-07 21:11:45 +0800

    Add files via upload

---
+++ gui.ps1
@@ -0,0 +1,30 @@
+[Form] $Window = & {
+    [OutputType([Form])]
+
+    [Form] $F = [Form]::new()
+    $F.Text = [Settings]::Title
+    $F.Size = [Size]::new([Settings]::WindowSize, [Settings]::WindowSize)
+    $F.BackColor = [Color]::Black
+    $F.Icon = "assets\icon.ico"
+
+    return $F
+}
+
+
+[Timer] $GameLoop = CreateTimer 10
+[Timer] $SpawnTimer = CreateTimer ([Settings]::SpawnRate)
+
+
+[Label] $Score = & {
+    [OutputType([Label])]
+
+    [Label] $L = [Label]::new()
+    $L.Width, $L.Height = $Window.Width, 30
+    $L.BackColor = [Color]::Transparent
+    $L.ForeColor = [Color]::White
+    $L.Font = [Font]::new("Arial", 15, [FontStyle]::Bold)
+
+    return $L
+}
+
+$Window.Controls.Add($Score)
---
+++ settings.ps1
@@ -0,0 +1,14 @@
+class Settings{
+    static [String] $Title = "Space Shooters";
+    static [Int] $WindowSize = 600
+
+    static [Int] $EntitySize = 60
+    static [Int] $EntitySpeed = 5
+    static [Int] $SpawnRate = 500
+    static [int] $ProjectileWidth = 20
+    static [Int] $ProjectileSpeed = 10
+
+    static [HashTable] $Assets = @{}
+
+    static [Int] $Score = 0
+}
